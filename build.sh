8e9554a4cb64eb37273fba22c858b6a36ce64545
lufinkey <luisfinke@gmail.com>
2017-05-15 15:03:13 -0400

    use fakeroot to force deb contents to be root

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-dpkg -b kodi-openbox
+if ! type "fakeroot" > /dev/null
+then
+	>&2 echo "You must install the package \"fakeroot\" in order to correctly build this DEB file"
+	exit 1
+fi
+fakeroot dpkg -b kodi-openbox
