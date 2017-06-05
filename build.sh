983fd2d91b45a5b8e355a9446f6b3199b80b20ab
Luis Finke <luisfinke@gmail.com>
2017-06-05 00:23:39 -0400

    why would you be building this for a non-debian system
    I'm a dumbass

--- build.sh
+++ build.sh
@@ -2,18 +2,6 @@
 if ! type "fakeroot" > /dev/null
 then
 	echo "You must install the package \"fakeroot\" in order to correctly build this DEB file"
-	if type "apt-get"
-	then
-		sudo apt-get install -y "fakeroot" || exit 1
-	elif type "apt"
-	then
-		sudo apt install -y "fakeroot" || exit 1
-	elif type "yum"
-	then
-		sudo yum install -y "fakeroot" || exit 1
-	else
-		>&2 echo "I don't know which package manager you have so you'll have to install it yourself"
-		exit 1
-	fi
+	sudo apt-get install -y "fakeroot" || exit 1
 fi
 fakeroot dpkg -b kodi-openbox
