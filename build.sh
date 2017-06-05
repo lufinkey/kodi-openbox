2fee9d9763fb60c32f3b61fe96182d6fce5e5ab0
Luis Finke <luisfinke@gmail.com>
2017-06-05 00:08:11 -0400

    install fakeroot for the lazy fucks out there

--- build.sh
+++ build.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 if ! type "fakeroot" > /dev/null
 then
-	>&2 echo "You must install the package \"fakeroot\" in order to correctly build this DEB file"
-	exit 1
+	echo "You must install the package \"fakeroot\" in order to correctly build this DEB file"
+	if type "apt-get"
+	then
+		sudo apt-get install -y "fakeroot" || exit 1
+	elif type "apt"
+	then
+		sudo apt install -y "fakeroot" || exit 1
+	elif type "yum"
+	then
+		sudo yum install -y "fakeroot" || exit 1
+	else
+		>&2 echo "I don't know which package manager you have so you'll have to install it yourself"
+		exit 1
+	fi
 fi
 fakeroot dpkg -b kodi-openbox
